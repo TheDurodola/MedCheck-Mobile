9b703491fc404cdcc231ac941df7c804e67e42e8
Durodola Abolaji <bolajidurodola@gmail.com>
2026-02-05 00:39:45 +0100

    progress

--- medcheck/lib/features/auth/data/repositories/auth_repository.dart
+++ medcheck/lib/features/auth/data/repositories/auth_repository.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
-
-import '../../../../core/errors/failures.dart';
-import '../../domain/entities/user_entity.dart';
-import '../../domain/usecases/login_params.dart';
-
+import 'package:medcheck/core/errors/failures.dart';
+import 'package:medcheck/features/auth/domain/entities/user_entity.dart';
+import 'package:medcheck/features/auth/domain/usecases/login_user.dart';
+import 'package:medcheck/features/auth/domain/usecases/sign_up_params.dart';
 
 abstract class AuthRepository {
   Future<Either<Failure, UserEntity>> loginUser(LoginParams params);
+  Future<Either<Failure, UserEntity>> signUpCustomer(SignUpParams params);
 }
